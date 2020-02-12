b27e3ae4882457a09d8dde607451e95f2640aa15
rajkstats <raj.k.stats@gmail.com>
2020-02-12 18:38:13 +0530

    non-dynamic favorites added

--- app.R
+++ app.R
@@ -20,13 +20,16 @@ library(shinyjs)
 library(data.table)
 
 source(file = "00_scripts/git_functions.R")
+source(file = "00_Scripts/info_card.R")
 
 #trending_repos <- trending_repos_on_github(language = "python", since = "weekly",gtoken = gtoken)
 #trending_repos <- tibble::rowid_to_column(trending_repos, "id")
 
 
-# FUNCTIONS ----
+# Application Auth ----
 github_app_auth()
+
+current_user_favorites <- c("AAPL", "GOOG", "NFLX")
 
 # FUNCTIONS ----
 navbar_page_with_inputs <- function(...) {
@@ -114,6 +117,7 @@ ui <- fluidPage(
     
     
     navbar_page_with_inputs(
+    
     # 2.1 Application Title ----
     title = div(
         tags$img(
@@ -127,19 +131,61 @@ ui <- fluidPage(
     collapsible = TRUE,
     
     theme = shinytheme("paper"),
-
+    
+    
     # PANEL START: Trending Repositories ----
     tabPanel(
         title = "Trending Repositories",
         
+        # CSS ----
+        #shinythemes::themeSelector(),
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+        ),
+        
         # JS ----
         shinyjs::useShinyjs(),
+  
+        
+        # HEADER -----    
 
+        # 2.0 FAVORITES ----
+        div(
+            class = "container hidden-sm hidden-xs",
+            id = "favorite_container",
+            
+            div(
+                class = "",
+                column(
+                    width = 12,
+                    h5(class= "pull-left","Favorites"),
+                    actionButton(inputId = "favorites_clear", "Clear Favorites", class = "pull-right"),
+                    actionButton(inputId = "favorites_toggle", "Show/Hide", class = "pull-right")
+                )
+            ),
+            div(
+                class = "container",
+                id    = "favorite_cards",
+                column(
+                    width =3,
+                    info_card(repo_name = "AiLearning", 
+                              repo_avatar = "https://avatars3.githubusercontent.com/u/24802038?v=4",
+                              repo_link = "https://github.com/apachecn/AiLearning")
+                ),
+                column(
+                    width =3,
+                    info_card(repo_name = "airflow", 
+                              repo_avatar = "https://avatars0.githubusercontent.com/u/47359?v=4",
+                              repo_link = "https://github.com/apache/airflow")
+
+                )
+            )
+        ),
         
         # Application UI ----
         div(
             id    = "application_ui",
-
+            class = "container",
             column(
                 width =3,
                 wellPanel(
@@ -148,7 +194,7 @@ ui <- fluidPage(
                         pickerInput(
                             inputId  = "language_selection",
                             label    = "Language",
-                            choices  = c("Python", "R", "JavaScript"), 
+                            choices  = c("R", "Python", "JavaScript"), 
                             multiple = FALSE,
                             selected = "R",
                             options = pickerOptions(
@@ -173,17 +219,15 @@ ui <- fluidPage(
                             actionButton(inputId = "settings_toggle", label = NULL, icon = icon("cog"))
                         )
                     ),
-                    
                     div(
                         id = "input_settings",
                         hr(), 
                         radioGroupButtons(inputId = 'var', label = 'Sort By',choices = c(Stars='stars',Forks='forks'),selected = 'stars'),
-                        #actionButton(inputId = "sort_stars", label = "Sort By",icon = icon("star")),
-                        #actionButton(inputId = "sort_forks", label = "Sort By",icon = icon("code-branch")),
                     ) %>% hidden()
                 )
             ),
             
+
             # 1.3.2 App Library ----
             div(
                 class = "container",
@@ -209,7 +253,6 @@ server <- function(input, output, session) {
         toggle(id = "input_settings", anim = TRUE)
     })
     
-    
     # Language Selection ----
     language <- eventReactive(input$get_trending,{
         input$language_selection
@@ -220,7 +263,6 @@ server <- function(input, output, session) {
         input$trend_frequency
     }, ignoreNULL = FALSE)
 
-    
     # Trending Repos ---- 
     trending_repos <- reactive({
         tr <- trending_repos_on_github(language = language(), since = trend_freq(),gtoken = gtoken)
@@ -230,8 +272,99 @@ server <- function(input, output, session) {
         
     })
     
+    # FAVORITES ----
     
-    # Render Trending Cards ----
+#     # 2.1 Reactive Values - User Favorites ----
+#     reactive_values <- reactiveValues()
+#     reactive_values$favorites_list <- current_user_favorites
+#     
+#     # 2.2 Add Favorites ----
+#     observeEvent(input$favorites_add, {
+#         new_symbol <- get_repo_from_user_input(input$stock_selection)
+#         reactive_values$favorites_list <- c(reactive_values$favorites_list, new_symbol) %>% unique()
+#     })
+#     
+#     # 2.3 Render Favorite Cards ----
+#     output$favorite_cards <-renderUI({
+#         
+#         if(length(reactive_values$favorites_list)>0){
+#             
+#             generate_favorite_cards(
+#                 favorites  = reactive_values$favorites_list,
+#                 from       = today() - days(180), 
+#                 to         = today(),
+#                 mavg_short = input$mavg_short,
+#                 mavg_long  = input$mavg_long
+#             )
+#             
+#         } else {
+#             reactive_values$favorites_list <- NULL
+#         }
+#         
+#     })
+#     
+#     # 2.4 Delete Favorites ----
+#     observeEvent(input$favorites_clear,{
+#         modalDialog(
+#             title = "Clear Favorites",
+#             size = "m",
+#             easyClose = TRUE,
+#             
+#             p("Are you sure you want to remove favorites?"),
+#             br(),
+#             div(
+#                 selectInput(inputId = "drop_list",
+#                             label = "Remove Single Favorite",
+#                             choices = reactive_values$favorites_list %>% sort()),
+#                 actionButton(inputId = "remove_single_favorite",
+#                              label   = "Clear Single",
+#                              class   = "btn-warning"),
+#                 actionButton(inputId = "remove_all_favorites",
+#                              label   = "Clear ALL Favorites",
+#                              class   = "btn-danger")
+#                 
+#             ),
+#             
+#             footer = modalButton("Exit")
+#         ) %>% showModal()
+#     })
+#     
+#     # 2.4.1 Clear Single ----
+#     observeEvent(input$remove_single_favorite,{
+#         
+#         
+#         reactive_values$favorites_list <- reactive_values$favorites_list %>%
+#             .[reactive_values$favorites_list != input$drop_list]
+#         
+#         
+#         updateSelectInput(session = session, 
+#                           inputId = "drop_list",
+#                           choices = reactive_values$favorites_list %>% sort()
+#         )
+#         
+#     })
+#     
+#     # 2.4.2 Clear All ----
+#     observeEvent(input$remove_all_favorites,{
+#         reactive_values$favorites_list <- NULL
+#         
+#         updateSelectInput(session = session, 
+#                           inputId = "drop_list",
+#                           choices = ""
+#         )
+#     })
+#     
+#     
+#     # 2.5 Show/Hide Favorites ----
+#     observeEvent(input$favorites_toggle,{
+#         shinyjs::toggle(id = "favorite_card_section", anim = TRUE, animType = "slide")
+#     })
+#     
+# }
+
+    
+    
+    # Render Trending Repositories Cards ----
     output$output_cards <- renderUI({
         
         div(
